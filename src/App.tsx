d38a2fef3b67bf7cf318b5c4e2cb1cd12465b24f
Leonardo <leolousa@hotmail.com>
2025-08-30 00:19:56 -0300

    Acertando a formatação dos arquivo com prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import './styles/theme.css';
-import './styles/global.css';
-import { Heading } from './components/Heading/Heading';
-import { TimerIcon } from 'lucide-react';
+import './styles/theme.css'
+import './styles/global.css'
+import { Heading } from './components/Heading/Heading'
+import { TimerIcon } from 'lucide-react'
 
 export function App() {
   return (
     <>
-    <Heading>Olá Mundo!
-      <button>
-        <TimerIcon />
-      </button>
-    </Heading>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia consectetur numquam, quod eveniet et nemo quibusdam, sint fugiat earum fuga quasi repellat magnam quam commodi ad eligendi rerum laborum ut?</p>
+      <Heading>
+        Olá Mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia
+        consectetur numquam, quod eveniet et nemo quibusdam, sint fugiat earum
+        fuga quasi repellat magnam quam commodi ad eligendi rerum laborum ut?
+      </p>
     </>
   )
 }
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,9 +1,9 @@
 import styles from './Heading.module.css'
 
 type HeadingProps = {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
 export function Heading({ children }: HeadingProps) {
-    return <h1 className={styles.heading}>{children}</h1>
+  return <h1 className={styles.heading}>{children}</h1>
 }
