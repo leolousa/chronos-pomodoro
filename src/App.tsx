d68f2fdbb392d7f5d8644c2234d592d6c4dc8c15
Leonardo <leolousa@hotmail.com>
2025-08-31 15:21:37 -0300

    Iniciando novo componente

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,24 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <label htmlFor='input'>task</label>
+            <input id='input' type='text' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   )
 }
