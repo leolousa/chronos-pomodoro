5c70fae8c866490dfd04195667efb9808e105674
Leonardo <leolousa@hotmail.com>
2025-09-02 21:57:16 -0300

    Criando o componente MainTemplate e o componente Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
 import './styles/theme.css'
 import './styles/global.css'
-import { Container } from './components/Container'
-import { Logo } from './components/Logo'
-import { Menu } from './components/Menu'
-import { CountDown } from './components/CountDown'
-import { Footer } from './components/Footer'
-import { MainForm } from './components/MainForm'
+import { Home } from './pages/Home'
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <MainForm />
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  )
+  return <Home />
 }
